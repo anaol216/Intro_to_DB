dba822f2bef294ca3ce514e7f0536ac712bc04aa
anaol216 <atinafuanaol@gmail.com>
2025-10-19 14:25:35 +0300

    modified:   task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -2,5 +2,17 @@
 -- SQL script to print the full description (CREATE TABLE statement) 
 -- of the 'BOOKS' table in the current database.
 
-USE alx_book_store;
-SHOW CREATE TABLE Books;
+
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books'
+ORDER BY
+    ORDINAL_POSITION;
