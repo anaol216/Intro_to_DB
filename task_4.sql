0681ce054b8fac36b46d77e29abb5e9534ada797
anaol216 <atinafuanaol@gmail.com>
2025-10-19 14:23:19 +0300

    new file:   task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,6 @@
+-- task_4.sql
+-- SQL script to print the full description (CREATE TABLE statement) 
+-- of the 'BOOKS' table in the current database.
+
+USE alx_book_store;
+SHOW CREATE TABLE Books;
